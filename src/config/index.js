f966273f6fe3f4b939a1bf7432223316302288de
yaoxfly <1315892088@qq.com>
2021-05-24 14:24:16 +0800

    feat:配置项优化

--- src/config/index.js
+++ src/config/index.js
@@ -1,29 +1,33 @@
 /** @description 多环境配置项
  * @author yx
  */
-const config = {}
+
+// 公共配置
+const config = {
+  mock: false // 是否开启mock
+}
+
+// 不同环境下的配置
 const keyMap = {
   // 开发环境
   development: () => {
     Object.assign(config, {
-      server: '', // api地址
-      mock: false // 是否开启mock
+      server: '' // api地址
     })
   },
 
   // 测试环境
   test: () => {
     Object.assign(config, {
-      server: '', // api地址
-      mock: false // 是否开启mock
+      server: '' // api地址
     })
   },
 
   // 正式环境
   production: () => {
     Object.assign(config, {
-      server: '', // api地址
-      mock: false // 是否开启mock
+      server: '' // api地址
+
     })
   }
 }
